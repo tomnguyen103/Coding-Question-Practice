c845e60bc75280197160bc2323f5049bb4a1ead9
tomnguyen103 <huuthong103@gmail.com>
2020-01-08 12:09:11 -0800

    finished productSum

--- productSum.js
+++ productSum.js
@@ -15,18 +15,30 @@
 //     return result;
 // }
 
-function productSum(array,lev=1){
+// function productSum(array,lev=1){
+//     let result = 0;
+//     for(const element of array){
+//         if(Array.isArray(element)){
+//             result += productSum(element,lev+1);
+//             console.log("sum of special array result: "+result);
+//         }else{
+//             result += element;
+//             console.log("regular array result: " +result);
+//         }
+//     }
+//     return result*lev;
+// }
+
+function productSum(arr, lv=2) {
     let result = 0;
-    for(const element of array){
-        if(Array.isArray(element)){
-            result += productSum(element,lev+1);
-            console.log("sum of special array result: "+result);
+        for(let i=0; i<arr.length; i++){
+            if(Array.isArray(arr[i])){
+              result += productSum(arr[i],lv+1) * lv;
         }else{
-            result += element;
-            console.log("regular array result: " +result);
+            result += arr[i];
         }
     }
-    return result*lev;
+    return result;
 }
 
 console.log(productSum([5,2,[7,-1], 3, [6,[-13,8],4]]));
