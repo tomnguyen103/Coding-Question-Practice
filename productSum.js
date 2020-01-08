ca724496b3fbdee19ebe98351b1d1ecabc44dacd
tomnguyen103 <huuthong103@gmail.com>
2020-01-08 10:19:01 -0800

    udpated

---
+++ productSum.js
@@ -0,0 +1,32 @@
+// function productSum(array){
+//     let result = 0;
+//     for(let i=0; i<array.length;i++){
+//         let lev=1;
+//         var str = array.toString();
+//         if(str[i].charAt(0)=="["){
+//             lev++;
+//             result+= (lev*str[i])
+//         }else if(str[i].charAt(0)=="]"){
+//             lev=1;
+//         }else{
+//             result += array[i];
+//         }   
+//     }
+//     return result;
+// }
+
+function productSum(array,lev=1){
+    let result = 0;
+    for(const element of array){
+        if(Array.isArray(element)){
+            result += productSum(element,lev+1);
+            console.log("sum of special array result: "+result);
+        }else{
+            result += element;
+            console.log("regular array result: " +result);
+        }
+    }
+    return result*lev;
+}
+
+console.log(productSum([5,2,[7,-1], 3, [6,[-13,8],4]]));
