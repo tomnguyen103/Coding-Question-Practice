d6213b6ba03bf5728ed3f09a01f1f3ba4f0377c4
Tom Nguyen <huuthong103@gmail.com>
2020-01-21 04:48:04 -0800

    update

---
+++ DFS.js
@@ -0,0 +1,16 @@
+class Node {
+    constructor(name) {
+        this.name = name;
+        this.children = [];
+    }
+
+    addChild(name) {
+        this.children.push(new Node(name));
+        return this;
+    }
+
+    depthFirstSearch(array) {
+    // Write your code here.
+        
+    }
+}
