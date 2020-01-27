2e1199c140a7533555cb3fb07b54ead8c3a58bc4
Tom Nguyen <huuthong103@gmail.com>
2020-01-27 15:17:45 -0800

    finished practiced DFS

--- DFS.js
+++ DFS.js
@@ -8,9 +8,22 @@ class Node {
         this.children.push(new Node(name));
         return this;
     }
-
+    // O(v+e) time || O(v) space
     depthFirstSearch(array) {
     // Write your code here.
-        
+        array.push(this.name);
+        for(const child of this.children){
+            child.depthFirstSearch(array);
+        }
+        return array;
     }
 }
+
+const g = new Node('A');
+g.addChild('B').addChild('C').addChild('D');
+g.children[0].addChild('E').addChild('F');
+g.children[1].addChild('G').addChild('H').addChild('I');
+g.children[2].addChild('J').addChild('K');
+
+
+console.log(g.depthFirstSearch([]));
