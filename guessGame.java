5dde8475fd7b3fba95e340202f1b8d4f1aa4eec0
Tom Nguyen <huuthong103@gmail.com>
2020-03-01 22:52:57 -0800

    more algo

---
+++ guessGame.java
@@ -0,0 +1,23 @@
+/* The guess API is defined in the parent class GuessGame.
+    @param num, your guess
+    @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 0;
+        int right = n;
+        while (left<=right){
+            int middle = left + (right-left)/2;
+            int res = guess(middle);
+            if(res == 0){
+                return middle;
+            }else if(res < 0){
+                right = middle - 1;
+            }else{
+                left = middle + 1;
+            }
+        }
+        return 1;
+    }
+}
