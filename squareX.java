15cbce91026d08cd30193a7c4c80069f3aabbc4a
Tom Nguyen <huuthong103@gmail.com>
2020-03-01 21:52:58 -0800

    more algo

---
+++ squareX.java
@@ -0,0 +1,21 @@
+public int mySqrt(int x) {
+        if(x<2){
+            return x;
+        }
+        long num;
+        int left = 2;
+        int right = x/2;
+        int middle;
+        while(left<= right){
+            middle = left + (right-left)/2;
+            num = (long) middle * middle;
+            if(num>x){
+                right = middle - 1;
+            }else if(num<x){
+                left = middle + 1;
+            }else{
+                return middle;
+            }
+        }
+        return right;
+    }
