bb31ae5d95fa7452476e5472aa1d990e80d5d605
Tom Nguyen <huuthong103@gmail.com>
2020-03-24 01:20:59 -0700

    adding new problem;

---
+++ fizzbuzz.js
@@ -0,0 +1,17 @@
+function fizzbuzz(){
+    for(var i = 1; i< 101; i++){
+        var output = "";
+        if(i%3==0){
+            output += "Fizz";
+        }
+        if(i%5==0){
+            output += "Buzz";
+        }
+        if(output == ""){
+            output = i;
+        }
+        console.log(output);
+    }
+}
+
+fizzbuzz()
