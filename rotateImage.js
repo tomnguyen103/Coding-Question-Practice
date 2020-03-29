2f28cecfdeb2a65ec05357ac791073e453eb1816
Tom Nguyen <huuthong103@gmail.com>
2020-03-28 21:00:20 -0700

    more algo

--- rotateImage.js
+++ rotateImage.js
@@ -1,7 +1,8 @@
 function rotateMatrix(a){
+    var n = a.length;
     // Swap diagonally 
-    for(var i=0;i<a.length;i++){
-        for(var j=i;j<a.length;j++){
+    for(var i=0;i<n;i++){
+        for(var j=i;j<n;j++){
             var temp = a[i][j];
             a[i][j] = a[j][i];
             a[j][i] = temp;
@@ -9,12 +10,12 @@ function rotateMatrix(a){
     }
 
     // Swap columns in each row
-    for(var k=0; k<a.length;k++){
+    for(var k=0; k<n;k++){
         // Iterate to middle number and perform the swap
-        for(var l=0;l<(a.length/2);l++){
+        for(var l=0;l<(n/2);l++){
             var temp2 = a[k][l];
-            a[k][l] = a[k][a.length-1-l] // -l in case even number for each row
-            a[k][a.length-1-l] = temp2;
+            a[k][l] = a[k][n-1-l] // -l in case even number for each row
+            a[k][n-1-l] = temp2;
         }
     }
     return a;
