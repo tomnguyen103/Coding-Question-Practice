aacc3c5a1aafef1d0f2fe5e98373d26983fb8d53
Tom Nguyen <huuthong103@gmail.com>
2020-05-09 15:37:14 -0700

    more problem

---
+++ powerSet.js
@@ -0,0 +1,36 @@
+function powerset(array, index = null) {
+// Write your code here.
+    if(index===null){
+        index = array.length - 1;
+    }
+    if (index < 0){
+        return [[]];
+    }
+    const ele = array[index];
+    const subsets = powerset(array, index-1);
+    const length = subsets.length;
+    for (let i=0;i<length;i++){
+        const currentSub = subsets[i];
+        subsets.push(currentSub.concat(ele));
+    }
+    return subsets;
+}
+
+
+array = [1,2,3,4]
+console.log(powerset(array));
+
+/**
+ * input [1,2,3,4]
+ * output
+ * [
+ * [],          [ 1 ],
+ * [ 2 ],       [ 1, 2 ],
+ * [ 3 ],       [ 1, 3 ],
+ * [ 2, 3 ],    [ 1, 2, 3 ],
+ * [ 4 ],       [ 1, 4 ],
+ * [ 2, 4 ],    [ 1, 2, 4 ],
+ * [ 3, 4 ],    [ 1, 3, 4 ],
+ * [ 2, 3, 4 ], [ 1, 2, 3, 4 ]
+]
+ */
