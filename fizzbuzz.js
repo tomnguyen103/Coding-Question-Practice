8bb725d01cde2cf0356d19b954c9997044094b23
tomnguyen103 <huuthong103@gmail.com>
2021-11-26 10:43:12 -0600

    updated

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,16 @@
+function fizzbuzz3(){
+    for(var i=1;i<101;i++){
+        if(i%3==0 & i%5==0){
+            console.log("FizzBuzz");
+        }else if(i%3==0){
+            console.log("Fizz");
+        }else if(i%5==0){
+            console.log("Buzz");
+        }else{
+            console.log(i);
+        }
+    }
+}
 // function fizzbuzz(){
 //     for(var i =1; i<101; i++){
 //         if(i%3==0 & i%5==0){
@@ -12,6 +25,22 @@
 //         }
 //     }
 // }
+
+function fizzbizz2(){
+    for(var i = 1; i<101; i++){
+        var output = "";
+        if(i%3==0){
+            output +="Fizz";
+        }
+        if(i%5==0){
+            output +="Buzz";
+        }
+        if(output==""){
+            output = i;
+        }
+        console.log(output);
+    }
+}
 
 function fizzbuzz(){
     for(var i = 1; i< 101; i++){
